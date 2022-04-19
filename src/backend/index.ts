cd1d5fe9a08a7bed928a19f01c6a430d8b921519
Maitree Shakya <maitree.shakya@gmail.com>
2022-04-19 22:51:53 +1000

    ejs setup

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,7 +1,26 @@
 import express from "express";
+import path from "path";
+import bodyparser from "body-parser";
+import ejs from "ejs";
 
+const root = "/";
 const app = express();
-const PORT = 5000;
 
+//middleware
+app.use(bodyparser.json({ type: "*/*" }));
+
+//ejs setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.engine("html", ejs.renderFile);
+app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, "./build")));
+
+//ejs serving routes
+app.get(`${root}`, function (req, res) {
+	res.render("index", { root });
+});
+
+const PORT = 5000;
 app.listen(PORT);
 console.log("App is running at PORT: ", PORT);
